54705403194d8f9cbdb8e63ea4b80afb1ab1b86e
EC2 Default User <ec2-user@ip-172-31-71-70.ec2.internal>
2020-10-27 01:26:35 +0000

    initial commit

---
+++ hw0/hw0_1/hw0_1.cpp
@@ -0,0 +1,16 @@
+/*
+ * Title: hw0_1.cpp
+ * Abstract:
+ * Author: Dan Sedano
+ * ID: 0254
+ * Date: 10/26/2020
+ */ 
+ 
+ #include <iostream>
+ using namespace std;
+ 
+ int main() {
+     cout << "hello world" << endl;
+     
+     return 0;
+ }
