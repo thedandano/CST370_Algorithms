d805c6639d3592d78606dfa1bec650528ac00717
Dan Sedano <dandano2006@gmail.com>
2020-10-30 22:16:22 -0700

    finished hw3 passed all tests

--- hw1/hw1_3/hw1_3.cpp
+++ hw1/hw1_3/hw1_3.cpp
@@ -1,15 +1,63 @@
 /*
  * Title: hw1_3.cpp
- * Abstract: This program
+ * Abstract: This program takes in a set of events and displays the maximum
+ * number of events that can take place concurrently.
  * Author: Dan Sedano
  * ID: 0254
- * Date: 10/29/2020
+ * Date: 10/30/2020
  */
 
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main() {
-    
+int main()
+{
+    int numEvents;
+    cin >> numEvents;
+
+    int hours = 24;
+    int eventArr[numEvents][hours];
+    int start, end;
+
+    for (int x = 0; x < numEvents; x++)
+    {
+        cin >> start >> end;
+        for (int y = 0; y < hours; y++)
+        {
+            if (y >= start && y < end)
+            {
+                eventArr[x][y] = 1;
+            }
+            else
+            {
+                eventArr[x][y] = 0;
+            }
+        }
+    }
+
+    vector<int> maxCount;
+    int count = 0;
+    int max = 0;
+    for (int y = 0; y < hours; y++)
+    {
+        for (int x = 0; x < numEvents; x++)
+        {
+            if (eventArr[x][y] == 1)
+            {
+                count++;
+            }
+        }
+        // set max
+        if (count > max)
+        {
+            max = count;
+        }
+        //reset count
+        count = 0;
+    }
+    cout << "Max events:" << max << endl;
+
     return 0;
 }
