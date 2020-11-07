596622b4e2b373cd6fc62d888f2e018faf2fe4bd
Dan Sedano <dandano2006@gmail.com>
2020-11-07 15:51:30 -0800

    finished hw2_3

--- hw2/hw2_3/hw2_3.cpp
+++ hw2/hw2_3/hw2_3.cpp
@@ -14,61 +14,22 @@
 #include <iostream>
 #include <map>
 using namespace std;
-/**
- * This function prints out the binary form of a decimal number limited by the 
- * size of the set.
- * @param int n and int set_size
- */
-void printBits(int n, int set_size) {
-    int binaryNum[set_size] = {};
-    int i = set_size;
 
-    // if set is 0 then cannot convert to binary
-    if (set_size == 0) {
-        cout << set_size;
-    } else {
-        while (n > 0) {
-            binaryNum[i - 1] = n % 2;
-            n = n / 2;
-            i--;
-        }
-        for (auto x : binaryNum) {
-            cout << x;
-        }
-    }
-    cout << ":";
-}
+//A struct to hold the results of boxes chosen.
+struct Results {
+    unsigned int boxes;  // decimal
+    int maxApples;
+};
 
-/**
- * Prints out the power set and subsets of user input.
- * @param string *set 
- * @param int set_size
- * 
- */
-void printPowerSet(map <int,int> *table, int boxes) {
-    unsigned int pow_boxes = pow(2, boxes);
-    size_t bits = boxes;
-    bool is_empty = true;
-
-    for (int x = 0; x < pow_boxes; x++) {
-        printBits(x, boxes);
-        // checks the least significant bit first
-        for (int y = boxes - 1; y >= 0; y--) {
-            if (x & (1 << y)) {
-                cout << &table[y] << " ";
-                is_empty = false;
-            }
-        }
-        if (is_empty) {
-            cout << "EMPTY";
-        }
-        cout << endl;
-    }
-}
+// Function prototypes
+Results chooseBoxes(map<int, int> table, int boxes);
+int sumChosenBoxes(unsigned int number, map<int, int> table);
+void printResults(Results p);
 
 int main() {
     // create table
     map<int, int> table;
+    Results chosenBoxes;
 
     // input number of boxes on table
     int boxes;
@@ -81,13 +42,87 @@ int main() {
         table[x] = apples;
     }
 
-    printPowerSet(&table,boxes);
-
-    // diagnostic
-    // for (auto &b : table) {
-    //     cout << b.first << " : "
-    //          << b.second << endl;
-    // }
+    chosenBoxes = chooseBoxes(table, boxes);
+    printResults(chosenBoxes);
 
     return 0;
+}
+
+/**
+ * Choses the boxes and returns the results of the way to get the most boxes.
+ * @param map<int, int> table
+ * @param int boxes
+ * @returns Results p
+ */
+Results chooseBoxes(map<int, int> table, int boxes) {
+    unsigned int pow_boxes = pow(2, boxes);
+    Results r;  // stores results
+    // initialize
+    r.boxes = 0;
+    r.maxApples = 0;
+
+    int sum = 0;
+    for (int x = 0; x < pow_boxes; x++) {
+        sum = sumChosenBoxes(x, table);
+        if (sum > r.maxApples) {
+            r.boxes = x;
+            r.maxApples = sum;
+        }
+    }
+    return r;
+}
+
+/**
+ * Returns the total sum of chosen boxes.
+ * @param unsigned int number
+ * @param map<int, int> table
+ * @returns sum
+ */
+int sumChosenBoxes(unsigned int number, map<int, int> table) {
+    bitset<16> bit(number);  // convert decimal number to binary
+
+    int limit = 15;  // 16-1 bits // 16 errors out for some reason.
+    int sum = 0;
+    for (int x = 0; x <= limit; x++) {
+        // if true then it is a chosen box
+        if (bit.test(x)) {
+            // if chosen box is first in array then check the next box
+            if (x == 0) {
+                if (!bit.test(x + 1)) {
+                    sum += table[x];
+                }
+            }
+            // if chosen box is last in array check previous box
+            else if (x == limit) {
+                if (!bit.test(x - 1)) {
+                    sum += table[x];
+                }
+            }
+            // other wise check next and prev box for all other boxes
+            else {
+                if (!bit.test(x + 1) && !bit.test(x - 1)) {
+                    sum += table[x];
+                }
+            }
+        }
+    }
+    return sum;
+}
+
+/**
+ * Prints the results of the selection process.
+ * @param Results p
+ */
+void printResults(Results r) {
+    bitset<16> bit(r.boxes);
+    //print chosen boxes
+    cout << "Boxes:";
+    for (int x = 0; x <= 15; x++) {
+        if (bit.test(x)) {
+            cout << x << " ";
+        }
+    }
+    cout << endl;
+    // print max number of apples
+    cout << "Max Apples:" << r.maxApples << endl;
 }
