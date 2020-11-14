c1f4117568f5d88f87d9e97c4a983230186bc0c7
Dan Sedano <dandano2006@gmail.com>
2020-11-14 11:00:32 -0800

    Finished! Added a counter to parent function and passed it by reference to dfs to keep track of each visited node's order.

--- hw3/hw3_3/hw3_3.cpp
+++ hw3/hw3_3/hw3_3.cpp
@@ -12,16 +12,14 @@
 #include <vector>
 using namespace std;
 
-void printList(vector<vector<int>> g);
+// Prototypes
 vector<vector<int>> userInput();
 void startTraversal(vector<vector<int>> g, vector<int> &m);
-void dfs(vector<vector<int>> g, vector<int> &m, int &count);
+void dfs(vector<vector<int>> g, vector<int> &m, int curr, int &counter);
 void printMark(vector<int> m);
-void printAll(vector<vector<int>> g, vector<int> m) {
-    printList(g);
-    cout << endl;
-    printMark(m);
-}
+
+// Global Var
+//int counter = 0;  // tracks the order of node visited
 
 int main() {
     vector<vector<int>> graph;
@@ -29,24 +27,12 @@ int main() {
     int start = 0;
 
     graph = userInput();
-    //    printList(graph);
 
-    mark.resize(graph.size(), 0);
+    mark.resize(graph.size(), 0);  // resizes to mark vector to match graph's size
     startTraversal(graph, mark);
-    //printMark(mark);
-    printAll(graph, mark);
+    printMark(mark);
 
     return 0;
-}
-
-void printList(vector<vector<int>> g) {
-    for (int x = 0; x < g.size(); x++) {
-        cout << x;
-        for (int y = 0; y < g[x].size(); y++) {
-            cout << "->" << g[x][y];
-        }
-        cout << endl;
-    }
 }
 
 /**
@@ -67,6 +53,7 @@ vector<vector<int>> userInput() {
         g[source].push_back(destination);
     }
 
+    // sorts neighboring nodes inside vector in ascending order
     for (int x = 0; x < vertices; x++) {
         sort(g[x].begin(), g[x].end());
     }
@@ -74,9 +61,15 @@ vector<vector<int>> userInput() {
     return g;
 }
 
+/**
+ * Readies variables to start traversal of graph.
+ * @param vector<vector<int>> g represents the graph to be traversed
+ * @param vector<int> &m is the mark array passed in by reference.
+ */
 void startTraversal(vector<vector<int>> g, vector<int> &m) {
-    int count = 0;
-    dfs(g, m, count);
+    int curr = 0;
+    int counter = 0;
+    dfs(g, m, curr, counter);
 }
 
 /**
@@ -84,14 +77,12 @@ void startTraversal(vector<vector<int>> g, vector<int> &m) {
  * @param vector<vector<int>> g the graph to be searched.
  * @returns vector<int> mark which records the order of traversed vertices.
  */
-void dfs(vector<vector<int>> g, vector<int> &m, int &count) {
-    m[count] = count + 1;
-    for (int x = 0; x < g[count].size(); x++) {
-        int neighbor = g[count][x];
+void dfs(vector<vector<int>> g, vector<int> &m, int curr, int &counter) {
+    m[curr] = ++counter;
+    for (int x = 0; x < g[curr].size(); x++) {
+        int neighbor = g[curr][x];
         if (m[neighbor] == 0) {
-            cout << "neighbor" << neighbor << endl;
-
-            dfs(g, m, neighbor);
+            dfs(g, m, neighbor, counter);
         }
     }
 }
