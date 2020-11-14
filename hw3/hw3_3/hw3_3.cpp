8117bfb2b6f08198cba79f5382759ad752295602
Dan Sedano <dandano2006@gmail.com>
2020-11-13 22:58:36 -0800

    got most of the program done, I just need to make sure the mark array is updating properly. The order of calls is correct though.

--- hw3/hw3_3/hw3_3.cpp
+++ hw3/hw3_3/hw3_3.cpp
@@ -7,10 +7,101 @@
  * Date: 11/13/2020
  */
 
+#include <algorithm>
 #include <iostream>
+#include <vector>
 using namespace std;
 
+void printList(vector<vector<int>> g);
+vector<vector<int>> userInput();
+void startTraversal(vector<vector<int>> g, vector<int> &m);
+void dfs(vector<vector<int>> g, vector<int> &m, int &count);
+void printMark(vector<int> m);
+void printAll(vector<vector<int>> g, vector<int> m) {
+    printList(g);
+    cout << endl;
+    printMark(m);
+}
+
 int main() {
-    
+    vector<vector<int>> graph;
+    vector<int> mark;
+    int start = 0;
+
+    graph = userInput();
+    //    printList(graph);
+
+    mark.resize(graph.size(), 0);
+    startTraversal(graph, mark);
+    //printMark(mark);
+    printAll(graph, mark);
+
     return 0;
+}
+
+void printList(vector<vector<int>> g) {
+    for (int x = 0; x < g.size(); x++) {
+        cout << x;
+        for (int y = 0; y < g[x].size(); y++) {
+            cout << "->" << g[x][y];
+        }
+        cout << endl;
+    }
+}
+
+/**
+ * Intakes user input
+ * @returns vector<vector<int>> that represents a graph based on user input.
+ */
+vector<vector<int>> userInput() {
+    vector<vector<int>> g;
+    int vertices, edges;
+
+    cin >> vertices;
+    g.resize(vertices);
+
+    cin >> edges;
+    int source, destination;
+    for (int x = 0; x < edges; x++) {
+        cin >> source >> destination;
+        g[source].push_back(destination);
+    }
+
+    for (int x = 0; x < vertices; x++) {
+        sort(g[x].begin(), g[x].end());
+    }
+
+    return g;
+}
+
+void startTraversal(vector<vector<int>> g, vector<int> &m) {
+    int count = 0;
+    dfs(g, m, count);
+}
+
+/**
+ * Implements the Depth-First Search Algorithm
+ * @param vector<vector<int>> g the graph to be searched.
+ * @returns vector<int> mark which records the order of traversed vertices.
+ */
+void dfs(vector<vector<int>> g, vector<int> &m, int &count) {
+    m[count] = count + 1;
+    for (int x = 0; x < g[count].size(); x++) {
+        int neighbor = g[count][x];
+        if (m[neighbor] == 0) {
+            cout << "neighbor" << neighbor << endl;
+
+            dfs(g, m, neighbor);
+        }
+    }
+}
+
+/**
+ * Prints out the mark vector
+ * @param vector<int> m input
+ */
+void printMark(vector<int> m) {
+    for (int x = 0; x < m.size(); x++) {
+        cout << "Mark[" << x << "]:" << m[x] << endl;
+    }
 }
