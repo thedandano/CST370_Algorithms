2d6b9e64fcd27b9e013eb2b1ec39df950bc6eb0f
Dan Sedano <dandano2006@gmail.com>
2020-12-14 10:34:27 -0800

    made code a little easier to follow: added a struct to hold results and broke down some functions. Added comments

--- hw6/hw6_2/hw6_2.cpp
+++ hw6/hw6_2/hw6_2.cpp
@@ -1,165 +1,187 @@
 /*
  * Title: hw6_2.cpp
- * Abstract: This program simuates the collection of the maximum value of coins
- * on a board of n x m dimentions.
+ * Abstract: This program simulates the collection of the maximum value of coins
+ * on a board of n x m dimensions.
  * Author: Dan Sedano
  * ID: 0254
  * Date: 12/12/2020
  */
 
 #include <iostream>
-#include <vector>
 #include <utility>
+#include <vector>
 using namespace std;
 
-#define BLOCKED 2
+/**
+ * Struct to hold results of coin collection
+ * int max holds the max value of coins
+ * vector<vector<int>> valueBoard hold the calculated board values.
+ */
+struct coinResults {
+    int max;
+    vector<vector<int>> valueBoard;
+};
 
-void print(vector<vector<int>> B)
-{
-    for (int x = 1; x < B.size(); x++)
-    {
-        for (int y = 1; y < B[x].size(); y++)
-        {
+#define BLOCKED 2
+void print(vector<vector<int>> B);
+vector<vector<int>> inputBoard();
+coinResults collectCoins(vector<vector<int>> C);
+int max(int a, int b);
+vector<pair<int, int>> getPath(vector<vector<int>> F);
+void printPath(vector<pair<int, int>> path);
+
+int main() {
+    // input board
+    vector<vector<int>> board = inputBoard();
+    coinResults r = collectCoins(board);
+
+    // print results
+    cout << "Max coins:" << r.max << endl;
+    printPath(getPath(r.valueBoard));
+
+    return 0;
+}
+
+/**
+ * Prints the 2D vector. This is just for diag.
+ * @param vector<vector<int>> B
+ */
+void print(vector<vector<int>> B) {
+    for (int x = 1; x < B.size(); x++) {
+        for (int y = 1; y < B[x].size(); y++) {
             cout << B[x][y] << " ";
         }
         cout << endl;
     }
 }
 
-vector<vector<int>> inputBoard()
-{
+/**
+ * Receives user input data for the board.
+ * @returns vector<vector<int>> 
+ */
+vector<vector<int>> inputBoard() {
     vector<vector<int>> B;
     int height, width;
 
-    // input dimentions
+    // input dimensions
     cin >> height >> width;
     height++;
     width++;
     B.resize(width, vector<int>(height));
 
     //input data
-    for (int x = 1; x < B.size(); x++)
-    {
-        for (int y = 1; y < B[x].size(); y++)
-        {
+    for (int x = 1; x < B.size(); x++) {
+        for (int y = 1; y < B[x].size(); y++) {
             cin >> B[x][y];
         }
     }
     return B;
 }
 
-int max(int a, int b)
-{
-    if (a > b)
-        return a;
-    return b;
-}
+/**
+ * Performs calculates the values of the board using dynamic programming
+ * @param vector<vector<int>> &C the board with coins.
+ * @returns coinResults containing max value of coins and 2D vector containing calculated values.
+ */
+coinResults collectCoins(vector<vector<int>> C) {
+    int width = C.size();
+    int height = C[0].size();
+    vector<vector<int>> F(width, vector<int>(height));
 
-void getPath(vector<vector<int>> F)
-{
-    vector<pair<int, int>> path;
-    int x = F[0].size() - 1;
-    int y = F.size() - 1;
-
-    while (true)
-    {
-
-        path.push_back(make_pair(x, y));
-
-        if (x > 1 && y > 1)
-        {
-            if (F[y - 1][x] > F[y][x - 1])
-            {
-                y--;
-            }
-            else
-            {
-                x--;
-            }
-        }
-        else if (y > 1)
-        {
-            if (F[y - 1][x] >= F[y][x - 1])
-                y--;
-        }
-        else if (x > 1)
-        {
-            if (F[y - 1][x] <= F[y][x - 1])
-                x--;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    cout << "Path:"
-         << "(" << path[path.size() - 1].first
-         << "," << path[path.size() - 1].second << ")";
-
-    for (int x = path.size() - 2; x >= 0; x--)
-    {
-        cout << "->"
-             << "(" << path[x].first
-             << "," << path[x].second << ")";
-    }
-    cout << endl;
-}
-
-int collectCoins(vector<vector<int>> &C, vector<vector<int>> &F)
-{
     F[1, 1] = C[1, 1];
 
     // calculate first row
-    for (int j = 2; j < F[0].size(); j++)
-    {
-        if (C[1][j] == BLOCKED)
-        {
+    for (int j = 2; j < F[0].size(); j++) {
+        if (C[1][j] == BLOCKED) {
             F[1][j] = 0;
-        }
-        else
-        {
+        } else {
             F[1][j] = F[1][j - 1] + C[1][j];
         }
     }
 
     // calculate remaining rows
-    for (int i = 2; i < F.size(); i++)
-    {
-        if (C[i][1] == BLOCKED)
-        {
+    for (int i = 2; i < F.size(); i++) {
+        // looks at cell above
+        if (C[i][1] == BLOCKED) {
             F[i][1] = 0;
-        }
-        else
-        {
+        } else {
             F[i][1] = F[i - 1][1] + C[i][1];
         }
 
-        for (int j = 2; j < F[0].size(); j++)
-        {
-            if (C[i][j] == BLOCKED)
-            {
+        // looks at the cell before and above
+        for (int j = 2; j < F[0].size(); j++) {
+            if (C[i][j] == BLOCKED) {
                 F[i][j] = 0;
-            }
-            else
-            {
+            } else {
                 F[i][j] = max(F[i - 1][j], F[i][j - 1]) + C[i][j];
             }
         }
     }
+
+    // returns the last row and last cell which is the max value and the calculated values board.
     int row = C.size() - 1;
     int col = C[0].size() - 1;
-    return F[row][col];
+    coinResults r;
+    r.max = F[row][col];
+    r.valueBoard = F;
+    return r;  
 }
 
-int main()
-{
-    vector<vector<int>> board = inputBoard();
-    int width = board.size();
-    int height = board[0].size();
-    vector<vector<int>> F(width, vector<int>(height));
-    cout << "\nMax coins:" << collectCoins(board, F) << endl;
-    //print(F);
-    getPath(F);
-    return 0;
+/**
+ * Returns the max value of two numbers.
+ * @return int max
+ */
+int max(int a, int b) {
+    if (a > b)
+        return a;
+    return b;
+}
+
+/**
+ * Calculates the path to collect the ax amount of coins.
+ * @param vector<vector<int>> a calculated board.
+ * @returns vector<pair<int,int>> which contains the path (backwards).
+ */
+vector<pair<int, int>> getPath(vector<vector<int>> F) {
+    vector<pair<int, int>> path;
+    int x = F[0].size() - 1;
+    int y = F.size() - 1;
+
+    while (true) {
+        path.push_back(make_pair(x, y));  // adds current node to path.
+
+        if (x > 1 && y > 1) {
+            if (F[y - 1][x] > F[y][x - 1]) {
+                y--;
+            } else {
+                x--;
+            }
+        } else if (y > 1) {  // this occurs if x axis is at limit
+            if (F[y - 1][x] >= F[y][x - 1])
+                y--;
+        } else if (x > 1) {  // this occurs if y axis is at limit
+            if (F[y - 1][x] <= F[y][x - 1])
+                x--;
+        } else {
+            break;
+        }
+    }
+    return path;
+}
+
+/**
+ * Prints the path 
+ * @param vector<pair<int,int>> path
+ */
+void printPath(vector<pair<int, int>> path) {
+    cout << "Path:"
+         << "(" << path[path.size() - 1].first
+         << "," << path[path.size() - 1].second << ")";
+
+    for (int x = path.size() - 2; x >= 0; x--) {
+        cout << "->"
+             << "(" << path[x].first
+             << "," << path[x].second << ")";
+    }
+    cout << endl;
 }
